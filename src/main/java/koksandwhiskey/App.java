cb9f4b4201e10e1e8620453f508f181a8f26aa3e
roman <romandubovyi@gmail.com>
2017-02-13 20:35:11 +0200

    Initial commit

---
+++ src/main/java/koksandwhiskey/App.java
@@ -0,0 +1,13 @@
+package koksandwhiskey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
